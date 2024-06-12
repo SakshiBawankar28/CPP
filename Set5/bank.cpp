7d0e478120d46f8cccf0fc1251c20600df59a225
SakshiBawankar28 <sakshibawabkar@gmail.com>
2024-06-12 20:10:29 +0530

    CPP Programs

---
+++ Set5/bank.cpp
@@ -0,0 +1,2 @@
+// Bank Account Inheritance:
+// Problem Statement: Design a system for managing bank accounts. Create a base class BankAccount with attributes like account number and balance. Derive classes like SavingsAccount and CheckingAccount, each with specialized methods like withdraw() and calculate_interest().
